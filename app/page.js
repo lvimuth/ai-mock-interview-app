1e1e2166726fb4bf8a3bb6984d8a3ca9ed26dfd2
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-21 22:56:11 +0400

    project setup

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Home</h2>
+      <Button>Click Me</Button>
     </div>
   );
 }
