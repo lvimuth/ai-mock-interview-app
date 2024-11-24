8893e8146d4688a00cea3fe54a8bb657784fc1ce
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-24 19:01:05 +0400

    text to speech

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -1,7 +1,15 @@
-import { Lightbulb } from "lucide-react";
+import { Lightbulb, Volume2 } from "lucide-react";
 import React from "react";
 
 function QuestionsSection({ mockInterviewQuestion, activeQuestionIndex }) {
+  const textToSpeech = (text) => {
+    console.log(text);
+    if ("speechSynthesis" in window) {
+      const speech = new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(speech);
+    } else alert("Sorry, Your browser does not support speech");
+  };
+
   return (
     mockInterviewQuestion && (
       <div className="p-5 border rounded-lg my-10">
@@ -20,6 +28,13 @@ function QuestionsSection({ mockInterviewQuestion, activeQuestionIndex }) {
         <h2 className="my-5 text-sm md:text-md">
           {mockInterviewQuestion[activeQuestionIndex]?.question}
         </h2>
+        <Volume2
+          className="cursor-pointer"
+          onClick={() => {
+            console.log("clicked");
+            textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.question);
+          }}
+        />
         <div className="border border-blue-200 rounded-lg p-5 bg-blue-100 mt-20">
           <h2 className="flex gap-2 items-center text-primary">
             <Lightbulb />
--- app/dashboard/interview/[interviewId]/start/_components/RecordAnsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnsSection.jsx
@@ -1,12 +1,33 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Webcam from "react-webcam";
+import useSpeechToText from "react-hook-speech-to-text";
+import { Mic } from "lucide-react";
 
 function RecordAnsSection() {
+  const [userAnswer, setUserAnswer] = useState("");
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false,
+  });
+  useEffect(() => {
+    results.map((results, index) =>
+      setUserAnswer((prevAns) => prevAns + results?.transcript)
+    );
+  }, [results]);
+
   return (
-    <div className="flex items-center justify-center flex-col">
-      <div className="flex flex-col justify-center items-center bg-secondary rounded-lg p-5 m-10">
+    <div className="flex items-center justify-center flex-col gap-7">
+      <div className="flex flex-col justify-center items-center bg-secondary rounded-lg p-5 ml-10 mt-10">
         <Image
           src={"/webcam.svg"}
           width={50}
@@ -22,9 +43,22 @@ function RecordAnsSection() {
             zIndex: 10,
           }}
         />
-        
-          </div>
-          <Button variant='outline' className=''> Record Answer</Button>
+      </div>
+      <Button
+        variant="outline"
+        className=""
+        onClick={isRecording ? stopSpeechToText : startSpeechToText}
+      >
+        {" "}
+        {isRecording ? (
+          <h2 className="text-red-500 flex gap-2">
+            <Mic /> Stop Recording
+          </h2>
+        ) : (
+          "Record Answer"
+        )}
+      </Button>
+      <Button onClick={() => console.log(userAnswer)}>Show User Answer</Button>
     </div>
   );
 }
