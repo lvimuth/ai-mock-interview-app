21008d06ed0273a482fdb66c2d46c84f42fc58a5
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-24 14:22:45 +0400

    db insertion

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -13,14 +13,20 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { chatSession } from "@/utils/GeminiAIModel";
 import { LoaderCircle } from "lucide-react";
+import { MockInterview } from "@/utils/schema";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@clerk/nextjs";
+import moment from "moment/moment";
+import { db } from "@/utils/db";
 
 function AddNewInterview() {
   const [openDialog, setOpenDialog] = useState(false);
   const [jobPosition, setJobPosition] = useState("");
   const [jobDescription, setJobDescription] = useState("");
-  const [jobExperiences, setJobExperiences] = useState("");
+  const [jobExperience, setJobExperience] = useState("");
   const [loading, setLoading] = useState(false);
   const [jsonResponse, setJsonResponse] = useState([]);
+  const { user } = useUser();
 
   const onSubmit = async (event) => {
     setLoading(true);
@@ -29,9 +35,9 @@ function AddNewInterview() {
 
     console.log("Job Position:", jobPosition);
     console.log("Job Description:", jobDescription);
-    console.log("Years of Experience:", jobExperiences);
+    console.log("Years of Experience:", jobExperience);
 
-    const InputPrompt = `job position: ${jobPosition}, job description: ${jobDescription}, Years of Experience: ${jobExperiences}. Depends on this information please give me ${process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT} interview question with answers in json format . Give question and answers as field in JSON`;
+    const InputPrompt = `job position: ${jobPosition}, job description: ${jobDescription}, Years of Experience: ${jobExperience}. Depends on this information please give me ${process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT} interview question with answers in json format . Give question and answers as field in JSON`;
 
     const result = await chatSession.sendMessage(InputPrompt);
     const MockJsonResp = result.response
@@ -39,11 +45,36 @@ function AddNewInterview() {
       .replace("```json", "")
       .replace("```", "");
     console.log(JSON.parse(MockJsonResp));
+    setJsonResponse(MockJsonResp);
 
-      setJsonResponse(MockJsonResp);
-      
+    try {
+      if (MockJsonResp) {
+        const resp = await db
+          .insert(MockInterview)
+          .values({
+            mockId: uuidv4(),
+            jsonMockResp: MockJsonResp,
+            jobPosition: jobPosition,
+            jobDesc: jobDescription,
+            jobExperience: jobExperience,
+            createdBy: user?.primaryEmailAddress?.emailAddress,
+            createdAt: moment().format("DD-MM-YYYY"),
+          })
+          .returning({ mockId: MockInterview.mockId });
+        console.log("Inserted new mock interview: ", resp);
+      } else {
+        console.error(
+          "MockJsonResp is missing, cannot insert new mock interview"
+        );
+      }
+    } catch (error) {
+      console.error(
+        "An error occurred while inserting new mock interview:",
+        error
+      );
+    }
     // Close the dialog after form submission
-    // setOpenDialog(false);
+    setOpenDialog(false);
     setLoading(false);
   };
 
@@ -101,10 +132,8 @@ function AddNewInterview() {
                       max="25"
                       placeholder="Ex. 5"
                       required
-                      value={jobExperiences}
-                      onChange={(event) =>
-                        setJobExperiences(event.target.value)
-                      }
+                      value={jobExperience}
+                      onChange={(event) => setJobExperience(event.target.value)}
                     />
                   </div>
                 </div>
